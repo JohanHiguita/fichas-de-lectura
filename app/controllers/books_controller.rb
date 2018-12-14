3a8cb4e79fd683d0b9ae0913af30ff052c09353a
Johan Higuita <higuitajohan@gmail.com>
2018-12-14 13:04:02 -0500

    controlador y metodo show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,21 @@
 class BooksController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @books=Book.all
   end
 
   def show
+    @book=Book.find(params[:id])
+    @notes=@book.notes
+    @cites=@book.cites
+    @topics=@book.topics
+    @autor = "#{@book.autor.name1} #{@book.autor.name2} #{@book.autor.lastname1} #{@book.autor.lastname2}"
   end
 
   def new
+
   end
 
   def create
@@ -21,4 +29,14 @@ class BooksController < ApplicationController
 
   def delete
   end
+
+
+  private
+
+def book_params
+  params.require(:book).permit(:user_id, :title, :city,:editorial, :autor_id) #solo permite estos datos
+  
 end
+end
+
+
