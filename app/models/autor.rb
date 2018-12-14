7d7989839ffce7dfe216d26fe0dfe4dbff7ca4f6
Johan Higuita <higuitajohan@gmail.com>
2018-12-14 09:58:15 -0500

    Validations

--- app/models/autor.rb
+++ app/models/autor.rb
@@ -1,3 +1,4 @@
 class Autor < ApplicationRecord
 	belongs_to :book, optional: true # optional: true solo para correr seeds
+	validates :name1, :lastname1, presence: true
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :book, optional: true # optional: true solo para correr seeds
+  validates :content, presence: true
 end
