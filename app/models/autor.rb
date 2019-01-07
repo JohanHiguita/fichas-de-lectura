d9f400efd0ad516dac7ce7ea7ef88ed10dc99157
Johan Higuita <higuitajohan@gmail.com>
2019-01-07 00:39:50 -0500

    Capitalizar autor y Uppercase Título

--- app/models/autor.rb
+++ app/models/autor.rb
@@ -1,8 +1,20 @@
 class Autor < ApplicationRecord
 	belongs_to :book, optional: true # optional: true solo para correr seeds
 	validates :name1, :lastname1, presence: true
+	before_create :capitalize_fields
+	before_update :capitalize_fields
 
 	def name_with_initial
-    "#{self.lastname1}, #{self.name1}"
-  end
+    	"#{self.lastname1}, #{self.name1}"
+  	end
+
+  	private
+
+  	def capitalize_fields
+		self.name1.capitalize!
+		self.name2.capitalize!
+		self.lastname1.capitalize!
+		self.lastname2.capitalize!
+	end
+
 end
