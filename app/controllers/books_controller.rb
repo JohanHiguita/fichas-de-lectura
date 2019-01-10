bfd69b6bbd61f00855a4b1624e56e6e458701aad
Johan Higuita <higuitajohan@gmail.com>
2019-01-10 18:22:39 -0500

    Crear las notas personales

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -79,7 +79,7 @@ class BooksController < ApplicationController
 
 
   def book_params
-  params.require(:book).permit(:user_id, :title, :city,:editorial, :autor_id, :topic_ids) #solo permite estos datos
+  params.require(:book).permit(:user_id, :title, :city,:editorial, :notes, :autor_id, :topic_ids) #solo permite estos datos
   
   end
 end
