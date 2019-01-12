232ad731fa10bfe9a3cd19faf03fd6156d4486e8
Johan Higuita <higuitajohan@gmail.com>
2019-01-12 16:43:29 -0500

    Cambios generales y estilos devise

--- app/controllers/cites_controller.rb
+++ app/controllers/cites_controller.rb
@@ -1,2 +1,20 @@
 class CitesController < ApplicationController
+
+	def new
+		@cite = Cite.new
+		@book_id = params[:format] #con este key se recibe el dato pasado
+		
+	end
+
+	def create
+		# 
+		@cite = Cite.create(cite_params)
+
+	end
+
+	def cite_params
+  		params.require(:cite).permit(:content, :book_id) #solo permite estos datos
+	end
+
+
 end
