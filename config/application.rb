cb885a86d1b92ebcd4c022433922a4a02f62d2fc
Johan Higuita <higuitajohan@gmail.com>
2019-01-12 18:49:09 -0500

    Alertas, español y devise

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module FichasDeLectura
-  class Application < Rails::Application
+	class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -15,5 +15,7 @@ module FichasDeLectura
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+    config.i18n.default_locale = :es
+    config.i18n.enforce_available_locales = true
+end
 end
