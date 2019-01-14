f1679d604c06d9a1405d209d4e69c2b360d6ed41
Johan Higuita <higuitajohan@gmail.com>
2019-01-14 13:13:37 -0500

    Envio de correos

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,16 @@
+class UserNotifierMailer < ApplicationMailer
+	default from: "johanhiguita@fichas-lectura.com"
+
+	def welcome_email(user)
+		@user = user
+		@subject = "Se ha registrado en fichas-lectura"
+		mail(to: @user.email, subject: @subject)
+	end
+
+	def new_user_notification(user)
+		@user = user
+		@subject = "Fichas lectura: Alguien ha creado un usuario"
+		mail(to: "higuitajohan@gmail.com", subject: @subject)
+		
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-
   has_many :books, dependent: :destroy
+  after_create :send_mails
+
+  def send_mails
+  	#Correos al crear un nuevo usuario
+    UserNotifierMailer.welcome_email(self).deliver_now #Bienvenida al usuario
+    UserNotifierMailer.new_user_notification(self).deliver_now #notificación a higuitajohan@gmail.com
+  end
 end
